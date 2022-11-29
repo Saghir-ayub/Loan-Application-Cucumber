ef9c42ee4d000f4d015ca57fe4d2f988d0e6bf3b
Saghir Ayub <saghir.ayub@ten10.com>
2022-11-29 14:24:27 +0000

    setup driver factory

---
+++ src/test/java/Manager/DriverFactory.java
@@ -0,0 +1,27 @@
+package Manager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    static {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    private static void setDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized", "incognito");
+        driver = new ChromeDriver(options);
+    }
+
+    public static WebDriver getDriver(){
+        if(driver==null){
+            setDriver();
+        }
+        return driver;
+    }
+}

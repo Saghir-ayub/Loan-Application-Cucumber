94859c37c57275f0bd07df46388e6aa044b46fea
Saghir Ayub <saghir.ayub@ten10.com>
2022-11-30 09:57:14 +0000

    added json and html cucumber reporting

--- src/test/java/Manager/Runner.java
+++ src/test/java/Manager/Runner.java
@@ -5,6 +5,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Resources/Features", glue = {"StepDefinitions"},tags = "@LoggingInFeature",plugin = {"json:target/Cucumber-reports/Cucumber.json"})
+@CucumberOptions(features = "src/test/java/Resources/Features",
+        glue = {"StepDefinitions"},
+        tags = "@LoggingInFeature",
+        plugin = {"pretty",
+                "json:target/cucumber-reports/Cucumber.json",
+                "html:target/cucumber-reports/Cucumber-test-results.html"})
 public class Runner {
 }

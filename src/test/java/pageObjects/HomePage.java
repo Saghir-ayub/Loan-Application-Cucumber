73ae20a860315fe798c25b69ebecbe4636d9dd7e
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-06 12:34:42 +0000

    log4j logger added to HomePage, needs adding to other pages, also added log4j config file with default root level: info

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,14 +1,20 @@
 package pageObjects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage{
+    protected static final Logger logger = LogManager.getLogger(HomePage.class);
     public static final String URL = "http://13.40.4.230:8080/";
     private static final By LOGIN_BUTTON = By.xpath("//a[contains(@href,'logout')]");
     private static final By LOGGED_IN_AS = By.xpath("//a[contains(@href,'logout')]/parent::span");
     private static final By LOGOUT_CONFIRMATION = By.xpath("//form[contains(@action,'login')]//div[contains(@class,'alert-info')]");
 
-    public static void goToHomePage(){driver.get(URL);}
+    public static void goToHomePage(){
+        driver.get(URL);
+        logger.info("You have arrived on the homepage");
+    }
 
     public static void clickLoginButton() {
         WaitAndClick(LOGIN_BUTTON);
